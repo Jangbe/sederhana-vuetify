ac8d9351589af442077360738e05b61bcf5dc66e
Jangbe <jangebe91@gmail.com>
2021-01-18 11:23:03 +0700

    add carousel

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,32 +1,18 @@
 <template>
-<div>
-    <div class="container">
-        Ini halaman utama, {{ nama }}
-        <v-form action="#" @submit.prevent="changeName">
-        <v-text-field v-model="name"></v-text-field>
-        <v-btn type="submit">Change Name</v-btn>
-        </v-form>
-        
-    </div>
-</div>
+<v-container>
+    <v-carousel hide-delimiters :interval="3000" cycle show-arrows-on-hover>
+        <v-carousel-item v-for="(item,i) in 3" :key="i" :src="`/img/carousel/${i+1}.jpg`">
+
+        </v-carousel-item>
+    </v-carousel>
+</v-container>
 </template>
 
 <script>
 export default {
     data(){
         return {
-            name: ''
         }
     },
-    computed: {
-        nama(){
-            return this.$store.state.name
-        }
-    },
-    methods: {
-         changeName(){
-             this.$store.commit('changeName', this.name);
-         }
-    }
 }
 </script>
